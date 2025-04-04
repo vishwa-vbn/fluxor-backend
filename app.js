e6daf7e85094b73e4319d3cee822b61c41a46403
Vishwa <77456055+vishwa-vbn@users.noreply.github.com>
2025-04-04 14:34:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,19 +21,13 @@ const corsOptions = {
   },
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true, // Important for cookies/tokens!
+  
   optionsSuccessStatus: 204,
 };
 
 app.use(cors(corsOptions));
 
-app.options("*", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.sendStatus(204);
-});
+app.options("*", cors(corsOptions));
 
 
 app.use(express.json());
