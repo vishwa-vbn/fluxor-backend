582527e4589ac0c5384809176153f7f571f707ee
Vishwa Naik <vishwabnaik12@gmail.com>
2025-04-07 20:23:38 +0530

    seed

--- src/migrations/seed_premissions.sql
+++ src/migrations/seed_premissions.sql
@@ -14,3 +14,14 @@ INSERT INTO role_permissions (role_id, permission_id)
 VALUES
   (2, 1), (2, 2), (2, 3),  -- for 'user'
   (1, 1), (1, 2), (1, 3), (1, 4), (1, 5); -- for 'admin'
+
+
+INSERT INTO role_permissions (role_id, permission_id)
+VALUES
+
+  (1, 6)
+
+  INSERT INTO permissions (name, route, type, description)
+VALUES
+
+  ('categories', '/categories', 'frontend', 'manage categories');
